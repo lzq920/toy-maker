f36a0d435a232732bf70a4e9ae5e461feffb379b
eleven <lizhiqiang@meb.com>
2021-04-13 18:22:03 +0800

    update

--- src/components/common/box-model-setting/index.vue
+++ src/components/common/box-model-setting/index.vue
@@ -29,92 +29,13 @@ export default {
       borderBottomWidth: 0,
       borderLeftWidth: 0
     })
-    let setting = reactive({
-      text: '',
-      type: ''
-    })
-    const statusLeft = reactive({
-      show: false,
-      value: ''
-    })
-    const statusRight = reactive({
-      show: false,
-      value: ''
-    })
-    const statusTop = reactive({
-      show: false,
-      value: ''
-    })
-    const statusBottom = reactive({
-      show: false,
-      value: ''
-    })
-    const restStatus = () => {
-      statusLeft.show = false
-      statusRight.show = false
-      statusBottom.show = false
-      statusTop.show = false
-    }
-    const handleClick = (type) => {
-      const obj = {
-        1: '外边距',
-        2: '边框',
-        3: '内边距'
-      }
-      restStatus()
-      if (type !== 0) {
-        setting = {
-          type: type,
-          text: obj[type]
-        }
-      } else {
-        setting = {
-          type: false,
-          text: ''
-        }
-      }
-    }
-    const handleChecked = (type) => {
-      status`${type}`.value = getValue(type)
-    }
-    const getValue = (arrow) => {
-      let val = ''
-      switch (setting.type) {
-        case 1:
-          val = style[`margin${arrow}`]
-          break
-        case 2:
-          val = style[`border${arrow}Width`]
-          break
-        case 3:
-          val = style[`padding${arrow}`]
-          break
-        default:
-          val = ''
-          break
-      }
-      return val
-    }
-    const handleChange = (type) => {
-      switch (setting.type) {
-        case 1:
-          style[`margin${type}`] = Number(status`${type}`.value)
-          break
-        case 2:
-          style[`border${type}Width`] = Number(`status${type}`.value)
-          break
-        case 3:
-          style[`padding${type}`] = Number(status`${type}`.value)
-          break
-      }
-    }
     watch(props.config.styles, (val) => {
       style = merge(style, val)
     }, {
       deep: true,
       immediate: true
     })
-    return { handleClick, handleChange, handleChecked }
+    return { }
   }
 }
 </script>
