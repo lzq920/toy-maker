6c747e292790256b876b088af232dc1f8cbc5d78
eleven <lizhiqiang@meb.com>
2021-04-28 12:29:33 +0800

    feature:优化默认图片

--- src/enum/tip.js
+++ src/enum/tip.js
@@ -1,6 +1,6 @@
 export default {
   defaultText: '请填写文本',
-  defaultImage: '//via.placeholder.com/375x200?text=default+image',
+  defaultImage: '//via.placeholder.com/375x200?text=default+image+toy+maker',
   defaultSingleText: '我是单行文本区域，超出文本区域会自动显示省略号...',
   defaultMultipleText: '我是多行文本区域，超出文本区域会自动显示省略号，我是多行文本区域，超出文本区域会自动显示省略号...'
 }
