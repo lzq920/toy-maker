707c48cfc3b8d8326fca6f5a6fa248eb4378d8f5
eleven <lizhiqiang@meb.com>
2021-06-08 11:51:56 +0800

    feature:添加favicon

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <router-view :key="$route.fullPath"></router-view>
 </template>
 <script>
+import { useFavicon } from '@vueuse/core'
 export default {
   setup () {
-
+    const favicon = useFavicon()
+    favicon.value = 'favicon.icon'
   }
 }
 </script>
